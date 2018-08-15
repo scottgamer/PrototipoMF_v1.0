0099bd667834de1e72c6ab23c5217c6f44eb5c2c
scottgamer <richard.munoz@epn.edu.ec>
2018-08-15 18:47:40 -0500

    Applications section created

--- angularFE/src/app/app.component.ts
+++ angularFE/src/app/app.component.ts
@@ -9,11 +9,27 @@ import { Title }     from '@angular/platform-browser';
 })
 export class AppComponent {
   title = 'Discapacidad Visual';
-  
+  name = "Aplicacion";
+  category = "Baja visión"
+  starList: boolean[] = [true, true, true, true, true];
+  rating: number;
+
   public constructor(private titleService: Title ) { }
  
   public setTitle( newTitle: string) {
     this.titleService.setTitle( newTitle );
+  }
+
+  setStar(data: any) {
+    this.rating = data + 1;
+    for (var i = 0; i <= 4; i++) {
+      if (i <= data) {
+        this.starList[i] = false;
+      }
+      else {
+        this.starList[i] = true;
+      }
+    }
   }
 
   ngOnInit(){
