047a1c83dad2a566a81bcd1e41bdc10399e3d9d5
scottgamer <richard.munoz@epn.edu.ec>
2018-08-15 22:50:54 -0500

    New components added

--- angularFE/src/app/app.module.ts
+++ angularFE/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes} from '@angular/router';
 
 //import { AppBootstrapModule } from './app-bootstrap/app-bootstrap.module';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
@@ -7,17 +8,33 @@ import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ModalModule } from 'ngx-bootstrap/modal';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ApplicationsComponent } from './components/applications/applications.component';
+import { NewsComponent } from './components/news/news.component';
+import { EventsComponent } from './components/events/events.component';
 
+const appRoutes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'home', component:HomeComponent},
+  {path:'applications', component:ApplicationsComponent},
+  {path:'news', component:NewsComponent},
+  {path:'events', component:EventsComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ApplicationsComponent,
+    HomeComponent,
+    NewsComponent,
+    EventsComponent
   ],
   imports: [
     BrowserModule,
     BsDropdownModule.forRoot(),
     TooltipModule.forRoot(),
-    ModalModule.forRoot()
+    ModalModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     Title,
---
+++ angularFE/src/app/components/home/home.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  name = "Aplicacion";
+  category = "Baja visión"
+  starList: boolean[] = [true, true, true, true, true];
+  rating: number;
+
+  public constructor() { }
+
+  setStar(data: any) {
+    this.rating = data + 1;
+    for (var i = 0; i <= 4; i++) {
+      if (i <= data) {
+        this.starList[i] = false;
+      }
+      else {
+        this.starList[i] = true;
+      }
+    }
+  }
+
+  ngOnInit(){
+  }
+
+}
