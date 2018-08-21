73b57bcb10ede738c910cc738bf7a5afceddcc38
scottgamer <richard.munoz@epn.edu.ec>
2018-08-20 19:34:40 -0500

    Navbar added to user profile

--- angularFE/src/app/components/user/user.component.ts
+++ angularFE/src/app/components/user/user.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  fullName:string;
+  email:string;
+  password:string;
+  birthdayDate:string;
+  genre:string;
+  nationality:string;
+  bio:string;
+
+  constructor() { 
+    
+  }
 
   ngOnInit() {
+    this.fullName='Richard Munoz';
+    this.email='richosojason@msn.com';
+    this.password='**********';
+    this.birthdayDate='26/03/1993';
+    this.genre='Masculino';
+    this.nationality='Ecuatoriano';
+
+    console.log('se cargó');
+    
+    
   }
 
 }
