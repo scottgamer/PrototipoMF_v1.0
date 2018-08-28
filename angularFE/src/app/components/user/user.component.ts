e949e60180c4039b01589bfa51ac8e4461055c8f
scottgamer <richard.munoz@epn.edu.ec>
2018-08-27 21:29:08 -0500

    User profile sections added

--- angularFE/src/app/components/user/user.component.ts
+++ angularFE/src/app/components/user/user.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, TemplateRef } from '@angular/core';
-
+import { Component, OnInit, Output, TemplateRef } from '@angular/core';
 
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
@@ -11,34 +10,59 @@ import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 })
 export class UserComponent implements OnInit {
 
-  fullName:string;
-  email:string;
-  password:string;
-  birthdayDate:string;
-  genre:string;
-  nationality:string;
-  bio:string;
+  fullName: string;
+  email: string;
+  password: string;
+  birthdayDate: string;
+  genre: string;
+  nationality: string;
+  bio: string;
 
   modalRef: BsModalRef;
 
-  constructor(private modalService: BsModalService) { 
-    
+  apps: Application[];
+
+  constructor(private modalService: BsModalService) {
+    this.apps = [];
+
+    /* this.apps = [
+      {
+        name: 'app1',
+        category: 'Categoria 1',
+        starList: [true, true, true, true, true],
+        rating: 0
+      },
+      {
+        name: 'app2',
+        category: 'Categoria 1',
+        starList: [true, true, true, true, true],
+        rating: 0
+      }
+    ]; */
+
+    for (var i = 1; i < 4; i++) {
+      this.apps.push({
+        name: 'app ' + i,
+        img: 'app' + i + '.webp',
+        category: 'Categoria 1',
+        starList: [true, true, true, true, true],
+        rating: 0
+      });
+    }
+
   }
 
   ngOnInit() {
-    this.fullName='Richard Munoz';
-    this.email='richosojason@msn.com';
-    this.password='**********';
-    this.birthdayDate='26/03/1993';
-    this.genre='Masculino';
-    this.nationality='Ecuatoriano';
-
-    console.log('se cargó');
-    
+    this.fullName = 'Richard Munoz';
+    this.email = 'richosojason@msn.com';
+    this.password = '**********';
+    this.birthdayDate = '26/03/1993';
+    this.genre = 'Masculino';
+    this.nationality = 'Ecuatoriano';
   }
 
   editInfo(template: TemplateRef<any>) {
-    this.modalRef = this.modalService.show(template, {class: 'modal-sm'});
+    this.modalRef = this.modalService.show(template, { class: 'modal-sm' });
   }
 
   confirm(): void {
@@ -46,4 +70,33 @@ export class UserComponent implements OnInit {
     this.modalRef.hide();
   }
 
+  openModalApps(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+    console.log('abre modal');
+  }
+
 }
+
+
+interface Application {
+  name: string,
+  img:string,
+  category: string,
+  starList: boolean[],
+  rating: number
+}
+
+/* interface Application {
+  name: string,
+  img:string,
+  category: string,
+  starList: boolean[],
+  rating: number,
+  country: string,
+  developedBy: string,
+  version: number,
+  releaseDate: string,
+  platform: string,
+  androidMin: string,
+  appWebPage: string
+} */
